9090de0370807daa37c9dc0e8fb87068a9c35f29
parkyoungjin-888 <pyj8813@gmail.com>
2024-07-01 21:48:01 +0900

    service init : - service-config-manager - queue-kafka-consumer

---
+++ common/service-config-manager/app.py
@@ -0,0 +1,36 @@
+import grpc
+from concurrent import futures
+from config_module.proto import config_pb2
+from config_module.proto import config_pb2_grpc
+from config_module.config_loader import ConfigLoader
+
+_config_url = 'mongodb://yj:dudwls123@192.168.0.104:27017,192.168.0.104:27018/?replicaSet=rs0'
+_config_loader = ConfigLoader(config_db_path=_config_url,
+                              app_id='service-config-manager-001')
+
+
+class ConfigServer(config_pb2_grpc.ConfigServerServicer):
+    def GetConfig(self, request, context):
+        app_id = request.app_id
+        config = _config_loader.get_config(app_id)
+        unpack_config = _config_loader.unpack_config(config)
+
+        res_payload = map(lambda x: config_pb2.KeyValue(key=x['key'], value=x['value'], type=x['type']), unpack_config)
+        response = config_pb2.ConfigReply()
+        response.config_data.extend(list(res_payload))
+        return response
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    config_pb2_grpc.add_ConfigServerServicer_to_server(ConfigServer(), server)
+    server.add_insecure_port(f'[::]:{_config_loader.port}')
+    server.start()
+
+    print(f'{_config_loader.name} serve start : 0.0.0.0:{_config_loader.port}')
+
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
---
+++ message/queue-kafka-consumer/app.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from datetime import datetime
+
+from config_module.config_singleton import ConfigSingleton
+from kafka_module.kafka_consumer import KafkaConsumerControl
+from influxdb_module.influxdb_control import InfluxDbControl
+
+
+config = ConfigSingleton()
+config.load_config('localhost', 50050, 'queue-kafka-consumer-001')
+
+influxdb_control = InfluxDbControl(**config.get_value('influxdb_module'))
+kafka_consumer = KafkaConsumerControl(**config.get_value('kafka'))
+bucket = config.get_value('influxdb.bucket')
+
+
+def write_batch_data(messages: list[dict]):
+    df = pd.DataFrame(messages)
+    io_id_list = list(set(df['io_id']))
+    for io_id in io_id_list:
+        select_df = df[df['io_id'] == io_id]
+        data_list = list(select_df['value'])
+        datetime_list = [datetime.fromtimestamp(ts) for ts in select_df['timestamp']]
+        influxdb_control.insert_data(bucket, 'test', data_list, datetime_list, [io_id])
+
+
+if __name__ == "__main__":
+    kafka_consumer.start_consumer(write_batch_data)
