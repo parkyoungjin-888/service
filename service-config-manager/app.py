8ca1351ba1971647028bc14b2dc9d3e558b82692
parkyoungjin-888 <pyj8813@gmail.com>
2024-10-16 22:19:26 +0900

    환경 변수로 db에 연결할 수 있도록 수정 : service-config-manager

--- service-config-manager/app.py
+++ service-config-manager/app.py
@@ -1,19 +1,31 @@
+import os
 import grpc
 from concurrent import futures
 from config_module.proto import config_pb2
 from config_module.proto import config_pb2_grpc
 from config_module.config_loader import ConfigLoader
 
-_config_url = 'mongodb://yj:dudwls123@192.168.0.104:27017,192.168.0.104:27018/?replicaSet=rs0'
-_config_loader = ConfigLoader(config_db_path=_config_url,
-                              app_id='service-config-manager-001')
+
+# region ############################## config section ##############################
+
+local_config_url = 'mongodb://your-username:your-password@192.168.0.105:27017,192.168.0.105:27018/?replicaSet=rs0'
+local_app_id = 'service-config-manager-001'
+
+config_url = os.environ.get('CONFIG_URL') if os.environ.get('CONFIG_URL') else local_config_url
+app_id = os.environ.get('APP_ID') if os.environ.get('APP_ID') else local_app_id
+config_loader = ConfigLoader(config_db_path=config_url, app_id=app_id)
+print(f'config load data, url : "{config_url}", app_id : {app_id}')
+
+# endregion
+
+# region ############################## service define section ##############################
 
 
 class ConfigServer(config_pb2_grpc.ConfigServerServicer):
     def GetConfig(self, request, context):
         app_id = request.app_id
-        config = _config_loader.get_config(app_id)
-        unpack_config = _config_loader.unpack_config(config)
+        config = config_loader.get_config(app_id)
+        unpack_config = config_loader.unpack_config(config)
 
         res_payload = map(lambda x: config_pb2.KeyValue(key=x['key'], value=x['value'], type=x['type']), unpack_config)
         response = config_pb2.ConfigReply()
@@ -24,14 +36,15 @@ class ConfigServer(config_pb2_grpc.ConfigServerServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     config_pb2_grpc.add_ConfigServerServicer_to_server(ConfigServer(), server)
-    server.add_insecure_port(f'[::]:{_config_loader.port}')
+    server.add_insecure_port(f'[::]:{config_loader.port}')
     server.start()
 
-    print(f'{_config_loader.name} serve start : 0.0.0.0:{_config_loader.port}')
+    print(f'{config_loader.name} serve start : 0.0.0.0:{config_loader.port}')
 
     server.wait_for_termination()
+
+# endregion
 
 
 if __name__ == '__main__':
     serve()
-    print('3')
