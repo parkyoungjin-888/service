4996e870727f25b11a4d246ad86f6bf0d3b46db4
parkyoungjin-888 <pyj8813@gmail.com>
2025-03-16 23:28:49 +0900

    서비스 이름 변경 : grpc-config-server -> grpc-config-fetcher

--- grpc-config-fetcher/app.py
+++ grpc-config-fetcher/app.py
@@ -4,20 +4,19 @@ from concurrent import futures
 from config_module.proto import config_pb2
 from config_module.proto import config_pb2_grpc
 from config_module.config_loader import ConfigLoader
-import logging
 from utils_module.logger import LoggerSingleton
 
 
 # region ############################## config section ##############################
 
-local_config_url = 'mongodb://yj:dudwls123@192.168.0.104:27017'
-local_app_id = 'grpc-config-server-001'
+port = os.environ.get('CONFIG_URL', 21001)
 
-config_url = os.environ.get('CONFIG_URL') if os.environ.get('CONFIG_URL') else local_config_url
-app_id = os.environ.get('APP_ID') if os.environ.get('APP_ID') else local_app_id
-config_loader = ConfigLoader(config_db_path=config_url, app_id=app_id)
-logger = LoggerSingleton.get_logger('app_logger', file_name='./log/app.log', level=logging.DEBUG)
-logger.info(f'config load data, url : "{config_url}", app_id : {app_id}')
+local_config_url = f'mongodb://yj:dudwls123@192.168.0.104:27017'
+config_url = os.environ.get('CONFIG_URL', local_config_url)
+config_loader = ConfigLoader(config_db_path=config_url)
+
+logger = LoggerSingleton.get_logger('app_logger', file_name='./log/app.log', level='DEBUG')
+logger.info(f'config load data, config_url : {config_url}')
 
 # endregion
 
@@ -33,16 +32,17 @@ class ConfigServer(config_pb2_grpc.ConfigServerServicer):
         res_payload = map(lambda x: config_pb2.KeyValue(key=x['key'], value=x['value'], type=x['type']), unpack_config)
         response = config_pb2.ConfigReply()
         response.config_data.extend(list(res_payload))
+        logger.info({'app_id': app_id, 'res': config})
         return response
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     config_pb2_grpc.add_ConfigServerServicer_to_server(ConfigServer(), server)
-    server.add_insecure_port(f'[::]:{config_loader.port}')
+    server.add_insecure_port(f'[::]:{port}')
     server.start()
 
-    logger.info(f'{config_loader.name} serve start : 0.0.0.0:{config_loader.port}')
+    logger.info(f'config serve start : 0.0.0.0:{port}')
 
     server.wait_for_termination()
 
