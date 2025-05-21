68dff43dea5704e4390958925f6f08ab7f3fe0be
parkyoungjin-888 <pyj8813@gmail.com>
2025-05-21 23:31:41 +0900

    화면에 img 리스트 조회 기능 추가

---
+++ fastapi-images-manager/README.md
@@ -0,0 +1,24 @@
+# fastapi-images-manager
+
+images collection 을 관리하는 api 서버
+
+## Index
+
+- [Installation](#installation)
+- [History](#History)
+
+## Installation
+
+Instructions for setting up the project.
+
+```bash
+# Clone the repository
+git clone https://github.com/parkyoungjin-888/service.git
+
+# Install dependencies using Poetry
+cd fastapi-images-manager
+poetry install --no-root
+```
+
+## History
++ v0.1.0: init
---
+++ fastapi-images-manager/app.py
@@ -0,0 +1,60 @@
+import os
+import uvicorn
+from fastapi import FastAPI
+
+from config_module.config_singleton import ConfigSingleton
+from utils_module.logger import LoggerSingleton
+
+
+# region ############################## config section ##############################
+
+local_config_host = '192.168.0.104'
+local_config_port = 21001
+local_app_id = 'local-fastapi-images-manager'
+
+config_host = os.environ.get('CONFIG_HOST', local_config_host)
+config_port = int(os.environ.get('CONFIG_PORT', local_config_port))
+app_id = os.environ.get('APP_ID', local_app_id)
+
+config = ConfigSingleton()
+config.load_config(host=config_host, port=config_port, app_id=app_id)
+
+app_config = config.get_value('app')
+
+log_level = os.environ.get('LOG_LEVEL', 'DEBUG')
+logger = LoggerSingleton.get_logger(f'{app_config["name"]}.main', level=log_level)
+
+# endregion
+
+# region ############################## service define section ##############################
+
+import boto3
+from botocore.client import Config
+
+from data_model_module.model_cashe_manager import ModelCacheManager
+from fastapi_module import create_collection_router
+from routers.img_streaming_router import router as streaming_router
+
+minio_config = config.get_value('minio')
+s3_client = boto3.client('s3',
+                         endpoint_url=minio_config.get('endpoint'),
+                         aws_access_key_id=minio_config.get('access_key'),
+                         aws_secret_access_key=minio_config.get('secret_key'),
+                         config=Config(signature_version='s3v4'))
+
+data_model_config = config.get_value('data_model')
+data_model_name = data_model_config['model_name']
+model_manager = ModelCacheManager(s3_client, minio_config['bucket'], data_model_config['file_name'])
+data_model = model_manager.get_model(data_model_name)
+collection_router = create_collection_router(model_manager, data_model)
+
+app = FastAPI()
+app.include_router(collection_router, prefix=app_config['api_prefix'])
+app.include_router(streaming_router, prefix='/images/streaming')
+
+# endregion
+
+
+if __name__ == '__main__':
+    logger.info({'message': 'server start', 'port': app_config['port']})
+    uvicorn.run('app:app', host="0.0.0.0", port=app_config['port'])
