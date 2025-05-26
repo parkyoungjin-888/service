796d7a306bcba1474a2aaabed5aa41571c2159a9
parkyoungjin-888 <pyj8813@gmail.com>
2025-05-26 19:29:14 +0900

    init : fastapi-images-manager

--- fastapi-images-manager/app.py
+++ fastapi-images-manager/app.py
@@ -33,7 +33,7 @@ from botocore.client import Config
 
 from data_model_module.model_cashe_manager import ModelCacheManager
 from fastapi_module import create_collection_router
-from routers.html_router import router as html_router
+from routers.html_router import create_router as create_html_router
 
 minio_config = config.get_value('minio')
 s3_client = boto3.client('s3',
@@ -41,42 +41,18 @@ s3_client = boto3.client('s3',
                          aws_access_key_id=minio_config.get('access_key'),
                          aws_secret_access_key=minio_config.get('secret_key'),
                          config=Config(signature_version='s3v4'))
-
 data_model_config = config.get_value('data_model')
 data_model_name = data_model_config['model_name']
 data_model_bucket = minio_config['data_model_bucket']
 model_manager = ModelCacheManager(s3_client, data_model_bucket, data_model_config['file_name'])
 data_model = model_manager.get_model(data_model_name)
+
 collection_router = create_collection_router(model_manager, data_model)
+html_router = create_html_router(s3_client)
 
 app = FastAPI()
 app.include_router(collection_router, prefix=app_config['api_prefix'])
 app.include_router(html_router, prefix=app_config['api_prefix'])
-
-
-from fastapi import FastAPI, HTTPException, Query
-from fastapi.responses import StreamingResponse
-from botocore.exceptions import ClientError
-import io
-import mimetypes
-
-@app.get("/images/display")
-async def display_image(img_path: str = Query(..., description="MinIO의 객체 경로")):
-    try:
-        obj = s3_client.get_object(Bucket='images', Key=img_path)
-        content = obj['Body'].read()
-
-        # MIME 타입 추론
-        mime_type, _ = mimetypes.guess_type(img_path)
-        mime_type = mime_type or 'application/octet-stream'
-
-        return StreamingResponse(io.BytesIO(content), media_type=mime_type)
-
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'NoSuchKey':
-            raise HTTPException(status_code=404, detail="파일을 찾을 수 없습니다.")
-        else:
-            raise HTTPException(status_code=500, detail="MinIO 오류 발생")
 
 # endregion
 
--- fastapi-images-manager/routers/html_router.py
+++ fastapi-images-manager/routers/html_router.py
@@ -1,8 +1,12 @@
 import os
+import base64
+import mimetypes
+from typing import Optional
 from collections import OrderedDict
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Query
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from botocore.exceptions import ClientError
 
 from config_module.config_singleton import ConfigSingleton
 from utils_module.logger import LoggerSingleton
@@ -14,51 +18,77 @@ app_config = config.get_value('app')
 log_level = os.environ.get('LOG_LEVEL', 'DEBUG')
 logger = LoggerSingleton.get_logger(f'{app_config["name"]}.api', level=log_level)
 
-router = APIRouter()
-templates = Jinja2Templates(directory='templates')
 
+def create_router(s3_client):
+    router = APIRouter()
+    templates = Jinja2Templates(directory='templates')
 
-@router.get('/home', response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse('main.html', {'request': request})
+    @router.get('/home', response_class=HTMLResponse)
+    async def home(request: Request):
+        return templates.TemplateResponse('main.html', {'request': request})
 
+    @router.get('/table', response_class=HTMLResponse)
+    async def render_table(request: Request):
+        table_fields = OrderedDict([
+            ('_id', 'ID'),
+            ('device_id', 'Device'),
+            ('name', 'Name'),
+            ('event_datetime', 'Event Time'),
+        ])
 
-@router.get('/table', response_class=HTMLResponse)
-async def render_table(request: Request):
-    table_fields = OrderedDict([
-        ('_id', 'ID'),
-        ('device_id', 'Device'),
-        ('name', 'Name'),
-        ('event_datetime', 'Event Time'),
-    ])
+        project_model_name = 'ProjectImage'
+        sort = '-event_datetime'
+        page_size = 10
+        get_list_url = f'/images/list?project_model_name={project_model_name}&sort={sort}&page_size={page_size}'
 
-    project_model_name = 'ProjectImage'
-    sort = '-event_datetime'
-    page_size = 10
-    get_list_url = f'/images/list?project_model_name={project_model_name}&sort={sort}&page_size={page_size}'
+        tag_fields = ['device_id']
+        get_tag_url = f'/images/tag?fields={'&fields='.join(tag_fields)}'
+        blank_tag_texts = {'device_id': 'All Device'}
 
-    tag_fields = ['device_id']
-    get_tag_url = f'/images/tag?fields={'&fields='.join(tag_fields)}'
-    blank_tag_texts = {'device_id': 'All Device'}
+        return templates.TemplateResponse('list_table.html', {
+            'request': request,
+            'get_list_url': get_list_url,
+            'get_tag_url': get_tag_url,
+            'table_fields': table_fields,
+            'field_order': list(table_fields.keys()),
+            'tag_fields': tag_fields,
+            'blank_tag_texts': blank_tag_texts
+        })
 
-    return templates.TemplateResponse('list_table.html', {
-        'request': request,
-        'get_list_url': get_list_url,
-        'get_tag_url': get_tag_url,
-        'table_fields': table_fields,
-        'field_order': list(table_fields.keys()),
-        'tag_fields': tag_fields,
-        'blank_tag_texts': blank_tag_texts
-    })
+    @router.get('/display', response_class=HTMLResponse)
+    async def display_image(img_path: Optional[str] = Query(None)):
+        if not img_path:
+            return HTMLResponse(
+                "<p style='text-align:center;margin-top:20px;'>No Image</p>",
+                status_code=200
+            )
 
+        try:
+            obj = s3_client.get_object(Bucket='images', Key=img_path)
+            content = obj['Body'].read()
 
-# @router.get('/display', response_class=HTMLResponse)
-# async def display_image(img_path: str = ""):
-#     print('here')
-#     return f"""
-#     <html>
-#     <body>
-#         <img src="{img_path}" style="max-width:100%; max-height:100%;">
-#     </body>
-#     </html>
-#     """
+            mime_type, _ = mimetypes.guess_type(img_path)
+            mime_type = mime_type or 'application/octet-stream'
+
+            if not mime_type.startswith("image/"):
+                return HTMLResponse(
+                    f"<p style='text-align:center;margin-top:20px;'>이미지 파일이 아닙니다: {img_path}</p>",
+                    status_code=200
+                )
+            # base64로 인라인 이미지 표시
+            b64_image = base64.b64encode(content).decode("utf-8")
+            return HTMLResponse(
+                f"""
+                <div style="text-align:center;margin-top:10px;">
+                    <img src="data:{mime_type};base64,{b64_image}" alt="이미지" style="max-width:100%;max-height:95vh;">
+                </div>
+                """,
+                status_code=200
+            )
+        except ClientError as e:
+            return HTMLResponse(
+                "<p style='text-align:center;margin-top:20px;color:red;'>이미지를 불러올 수 없습니다.</p>",
+                status_code=200
+            )
+
+    return router
